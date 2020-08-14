58e9f8d7b20246f45cb42305ca1c7a07bff6650f
D-Zzavala <65042896+D-Zzavala@users.noreply.github.com>
2020-08-13 22:38:14 -0600

    5 metodos para modificar

--- Trying/src/trying/Trying.java
+++ Trying/src/trying/Trying.java
@@ -5,18 +5,60 @@
  */
 package trying;
 
-/**
- *
- * @author 50431
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Trying {
 
-    /**
-     * @param args the command line arguments
-     */
-    
+    static Scanner read = new Scanner(System.in);
+    static boolean Tf = false;
+    static ArrayList list = new ArrayList();
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        while (Tf) {
+            System.out.println("Opciones:");
+            System.out.println("1. Add");
+            System.out.println("2. List");
+            System.out.println("3. Remove");
+            System.out.println("0. Salir");
+            int op = read.nextInt();
+            switch (op) {
+                case 1: {
+                    System.out.println("Add");
+                    break;
+                }
+                case 2: {
+                    System.out.println("List");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Remove");
+                    break;
+                }
+                case 0:
+                    System.out.println("Salir");
+                    break;
+            }
+        }
+    }
+    
+    static void mod(){
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     }
     
 }
---
+++ Trying/src/trying/Zavala.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trying;
+
+import static trying.Trying.Tf;
+
+/**
+ *
+ * @author 50431
+ */
+public class Zavala {
+    
+     while (Tf) {
+            System.out.println("Opciones:");
+            System.out.println("1. Add");
+            System.out.println("2. List");
+            System.out.println("3. Remove");
+            System.out.println("0. Salir");
+            int op = read.nextInt();
+            switch (op) {
+                case 1: {
+                    System.out.println("Add");
+                    as
+                            s
+                            sd
+                                    f
+                                    g
+                                            gg
+                                            ew
+                                                    w
+                                                    q
+                                                            qw
+                                                            
+                    break;
+                }
+                case 2: {
+                    System.out.println("List");
+                    s
+                            s
+                            ss
+                                    s
+                                    s
+                                            
+                                            s
+                                            s
+                                                    S
+                                                    s
+                                                            s
+                                                            S
+                                                                    s
+                                                                    s
+                                                                            ss
+                                                                            
+                                                                            s
+                                                                                    s
+                                                                                    s
+                    break;
+                }
+                case 3: {
+                    System.out.println("Remove");
+                    break;
+                }
+                case 0:
+                    System.out.println("Salir");
+                    break;
+            }
+        }    
+    
+}
